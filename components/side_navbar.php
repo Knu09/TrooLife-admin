f0cdf0e00ca28c12cea012e3c54f6cce77ae7ea4
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-05-14 22:01:01 +0800

    Update: route in relevant

--- components/side_navbar.php
+++ components/side_navbar.php
@@ -178,7 +178,7 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="add_accounts.php">
+                            <a class="nav-link" href="../pages/add_accounts.php">
                                 <span class="sidebar-text"
                                     >Add Accounts</span
                                 >
